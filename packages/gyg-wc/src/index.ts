f3e709e4dcc12c62d05d5f720ec606e16c689e99
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 05:02:05 +1000

    feat(gyg-wc): add `gyg-search`

--- packages/gyg-wc/src/index.ts
+++ packages/gyg-wc/src/index.ts
@@ -2,6 +2,7 @@ import {
   GetYourGuide,
   GetYourGuideAvailability,
   GetYourGuideLocation,
+  GetYourGuideSearch,
 } from "./wc";
 
 export {};
@@ -9,4 +10,5 @@ export {};
 customElements.get("gyg-availability") ||
   customElements.define("gyg-availability", GetYourGuideAvailability);
 customElements.get("gyg-location") || customElements.define("gyg-location", GetYourGuideLocation);
+customElements.get("gyg-search") || customElements.define("gyg-search", GetYourGuideSearch);
 customElements.get("gyg-wc") || customElements.define("gyg-wc", GetYourGuide);
