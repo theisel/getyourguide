67f46061c323c77d23615f84b2783bd053b026e3
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 04:59:37 +1000

    refactor(gyg-wc)!: drop `gyg-activities`

--- packages/gyg-wc/src/index.ts
+++ packages/gyg-wc/src/index.ts
@@ -1,14 +1,11 @@
 import {
   GetYourGuide,
-  GetYourGuideActivities,
   GetYourGuideAvailability,
   GetYourGuideCity,
 } from "./wc";
 
 export {};
 
-customElements.get("gyg-activities") ||
-  customElements.define("gyg-activities", GetYourGuideActivities);
 customElements.get("gyg-availability") ||
   customElements.define("gyg-availability", GetYourGuideAvailability);
 customElements.get("gyg-city") || customElements.define("gyg-city", GetYourGuideCity);
