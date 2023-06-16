30e54eb901cfb078731d28f21788a02f15c4a8e6
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 06:33:38 +1000

    feat(sanity-plugin): add `search` schema

--- packages/sanity-plugin-getyourguide/lib/index.ts
+++ packages/sanity-plugin-getyourguide/lib/index.ts
@@ -1,6 +1,7 @@
 import { definePlugin } from "sanity";
 import availabilitySchema from "./schemas/availability";
 import locationSchema from "./schemas/location";
+import searchSchema from "./schemas/search";
 
 interface PluginOptions {
   partnerId: string;
@@ -14,6 +15,7 @@ export const getYourGuide = definePlugin<PluginOptions>((options) => ({
     types: [
       { ...availabilitySchema, options },
       { ...locationSchema, options },
+      { ...searchSchema, options },
     ],
   },
 }));
