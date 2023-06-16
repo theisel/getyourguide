408c8826010618460b3a8547ef73378e27aa3c5b
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 05:01:39 +1000

    feat(gyg-wc): add `gyg-location`

--- packages/gyg-wc/src/index.ts
+++ packages/gyg-wc/src/index.ts
@@ -1,10 +1,12 @@
 import {
   GetYourGuide,
   GetYourGuideAvailability,
+  GetYourGuideLocation,
 } from "./wc";
 
 export {};
 
 customElements.get("gyg-availability") ||
   customElements.define("gyg-availability", GetYourGuideAvailability);
+customElements.get("gyg-location") || customElements.define("gyg-location", GetYourGuideLocation);
 customElements.get("gyg-wc") || customElements.define("gyg-wc", GetYourGuide);
