584086b23b26ee5203be04f5638fe89f83dfad18
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 05:00:53 +1000

    refactor(gyg-wc)!: drop `gyg-city`

--- packages/gyg-wc/src/index.ts
+++ packages/gyg-wc/src/index.ts
@@ -1,12 +1,10 @@
 import {
   GetYourGuide,
   GetYourGuideAvailability,
-  GetYourGuideCity,
 } from "./wc";
 
 export {};
 
 customElements.get("gyg-availability") ||
   customElements.define("gyg-availability", GetYourGuideAvailability);
-customElements.get("gyg-city") || customElements.define("gyg-city", GetYourGuideCity);
 customElements.get("gyg-wc") || customElements.define("gyg-wc", GetYourGuide);
