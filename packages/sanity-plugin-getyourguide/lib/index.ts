ee4550dd6e0a6538ee5413716cb6a7decd143b38
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 06:33:02 +1000

    feat(sanity-plugin): add `location` schema

--- packages/sanity-plugin-getyourguide/lib/index.ts
+++ packages/sanity-plugin-getyourguide/lib/index.ts
@@ -1,5 +1,6 @@
 import { definePlugin } from "sanity";
 import availabilitySchema from "./schemas/availability";
+import locationSchema from "./schemas/location";
 
 interface PluginOptions {
   partnerId: string;
@@ -12,6 +13,7 @@ export const getYourGuide = definePlugin<PluginOptions>((options) => ({
     name: "getYourGuide",
     types: [
       { ...availabilitySchema, options },
+      { ...locationSchema, options },
     ],
   },
 }));
