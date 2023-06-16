0d4267a1fd7fdf9efbcf46a88ceba2456b9eeaeb
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 05:04:12 +1000

    refactor(gyg-wc)!: rewrite class

--- packages/gyg-wc/src/wc.ts
+++ packages/gyg-wc/src/wc.ts
@@ -1,11 +1,40 @@
+import { parseUrl } from "./utils";
+
 export class GetYourGuide extends HTMLElement {
   static SCRIPT_SRC = "https://widget.getyourguide.com/dist/pa.umd.production.min.js";
 
   private partnerId = "";
   private observer: MutationObserver;
 
-  protected get widget() {
+  protected get widgetType(): string {
     return this.getAttribute("widget") || "";
+  }
+
+  private get widget(): GetYourGuide {
+    let widgetInst: GetYourGuide | undefined;
+
+    if (this.hasAttribute("query")) {
+      widgetInst = new GetYourGuideSearch();
+    } else if (this.hasAttribute("url")) {
+      const tourId = parseUrl(this.getAttribute("url") || "")?.tourId;
+      widgetInst = tourId ? new GetYourGuideAvailability() : new GetYourGuideLocation();
+    } else if (this.hasAttribute("urls")) {
+      widgetInst = new GetYourGuideTours();
+    } else {
+      throw Error(`
+        GetYourGuide widget is missing one of the following attributes: 
+        - query (search)
+        - url (availability|location)
+        - urls (tours)
+      `);
+    }
+
+    for (let i = 0; i < this.attributes.length; i++) {
+      const { name, value } = this.attributes[i];
+      widgetInst.setAttribute(name, value);
+    }
+
+    return widgetInst;
   }
 
   constructor() {
@@ -29,67 +58,42 @@ export class GetYourGuide extends HTMLElement {
     this.render();
   }
 
-  private render() {
+  protected render() {
     if (this.hasAttribute("hidden")) {
       // Preload the script
       if (!document.head.querySelector(`link[href="${GetYourGuide.SCRIPT_SRC}"]`)) {
-        document.head.appendChild(this.createPreloadLink());
+        document.head.appendChild(this.providePreloadLinkElement());
       }
       return;
     }
 
     // Append the script
     if (!document.head.querySelector(`script[data-gyg-partner-id]`)) {
-      document.head.appendChild(this.createScript());
+      document.head.appendChild(this.provideScriptElement());
     }
 
     this.innerHTML = ""; // Clear the content
 
-    this.appendChild(this.createWidget());
+    "GYG-WC" === this.tagName
+      ? this.appendChild(this.widget.provideWidgetElement())
+      : this.appendChild(this.provideWidgetElement());
   }
 
-  protected createWidget(): HTMLElement {
-    let widgetInst: GetYourGuide | undefined;
-
-    switch (this.widget) {
-      case "activities":
-        widgetInst = new GetYourGuideActivities();
-        break;
-      case "availability":
-        widgetInst = new GetYourGuideAvailability();
-        break;
-      case "city":
-        widgetInst = new GetYourGuideCity();
-        break;
-    }
-
-    if (widgetInst) {
-      for (let i = 0; i < this.attributes.length; i++) {
-        const { name, value } = this.attributes[i];
-        if (name === "widget") continue;
-        widgetInst.setAttribute(name, value);
-      }
-      return widgetInst;
-    }
-
-    throw Error(`Unknown widget: ${this.widget}`);
-  }
-
-  protected createBaseWidget() {
+  protected provideWidgetElement() {
     const div = document.createElement("div");
 
     div.setAttribute(
       "data-gyg-href",
-      `https://widget.getyourguide.com/default/${this.widget}.frame`
+      `https://widget.getyourguide.com/default/${this.widgetType}.frame`
     );
-    div.setAttribute("data-gyg-widget", this.widget);
+    div.setAttribute("data-gyg-widget", this.widgetType);
     div.setAttribute("data-gyg-partner-id", this.partnerId);
     div.setAttribute("data-gyg-locale-code", this.lang || document.documentElement.lang);
 
     return div;
   }
 
-  private createPreloadLink() {
+  private providePreloadLinkElement() {
     const link = document.createElement("link");
 
     link.rel = "preload";
@@ -99,7 +103,7 @@ export class GetYourGuide extends HTMLElement {
     return link;
   }
 
-  private createScript() {
+  private provideScriptElement() {
     const script = document.createElement("script");
 
     script.async = true;
@@ -109,89 +113,124 @@ export class GetYourGuide extends HTMLElement {
 
     return script;
   }
-}
 
-export class GetYourGuideActivities extends GetYourGuide {
-  protected get widget() {
-    return "activities";
-  }
+  protected parseAttribute<T>(attr: string): T | string | null {
+    const value = ((val) => val && val.trim())(this.getAttribute(attr));
 
-  protected createWidget() {
-    const widget = this.createBaseWidget();
-    const type = this.getAttribute("type") || "";
-    const query = this.getAttribute("query") || "";
+    if (!value) return null;
 
-    switch (type) {
-      case "tours":
-        widget.setAttribute("data-gyg-tour-ids", query);
-        widget.setAttribute("data-gyg-number-of-items", query.split(",").length.toString());
-        return widget;
-      case "location":
-        widget.setAttribute("data-gyg-location-id", query);
-        break;
-      default:
-        widget.setAttribute("data-gyg-q", query);
+    try {
+      return JSON.parse(value);
+    } catch (err) {
+      return value;
     }
-
-    const size = this.getAttribute("size");
-
-    if (size) {
-      widget.setAttribute("data-gyg-number-of-items", size);
-    }
-
-    const exclude = this.getAttribute("exclude");
-
-    if (exclude) {
-      widget.setAttribute("data-gyg-excluded-tour-ids", exclude);
-    }
-
-    return widget;
   }
 }
 
 export class GetYourGuideAvailability extends GetYourGuide {
-  protected get widget() {
+  protected get widgetType() {
     return "availability";
-  }
-
-  protected createWidget() {
-    const widget = this.createBaseWidget();
-    const layout = this.getAttribute("layout") || "horizontal";
-    const currency = this.getAttribute("currency") || "";
-    const campaign = this.getAttribute("campaign") || "";
-
-    const theme = ((theme) => ("system" === theme ? this.systemTheme : theme))(
-      this.getAttribute("theme") || "system"
-    );
-
-    widget.setAttribute("data-gyg-theme", theme);
-    widget.setAttribute("data-gyg-variant", layout);
-    widget.setAttribute("data-gyg-tour-id", this.getAttribute("query") || "");
-
-    if (currency) {
-      widget.setAttribute("data-gyg-currency", currency);
-    }
-
-    if (campaign) {
-      widget.setAttribute("data-gyg-cmp", campaign);
-    }
-
-    return widget;
   }
 
   private get systemTheme() {
     return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
   }
+
+  protected provideWidgetElement() {
+    const widget = super.provideWidgetElement();
+
+    const url = parseUrl(this.getAttribute("url") || "");
+    const layout = this.getAttribute("layout") || "horizontal";
+    const currency = this.getAttribute("currency");
+    const campaign = this.getAttribute("campaign");
+
+    const theme = ((theme) => ("system" === theme ? this.systemTheme : theme))(
+      this.getAttribute("theme") || "system"
+    );
+
+    widget.setAttribute("data-gyg-tour-id", url?.tourId || "");
+    theme && widget.setAttribute("data-gyg-theme", theme);
+    layout && widget.setAttribute("data-gyg-variant", layout);
+    currency && widget.setAttribute("data-gyg-currency", currency);
+    campaign && widget.setAttribute("data-gyg-cmp", campaign);
+
+    return widget;
+  }
 }
 
-export class GetYourGuideCity extends GetYourGuide {
-  protected get widget() {
-    return "city";
+export class GetYourGuideLocation extends GetYourGuide {
+  protected get widgetType() {
+    return this.hasAttribute("size") ? "activities" : "city";
   }
 
-  protected createWidget() {
-    const widget = this.createBaseWidget();
-    widget.setAttribute("data-gyg-location-id", this.getAttribute("query") || "");
+  protected provideWidgetElement() {
+    const widget = super.provideWidgetElement();
+    const url = parseUrl(this.getAttribute("url") || "");
+
+    const locationId = url?.locationId;
+    const exclude = this.parseAttribute<string | string[]>("exclude");
+
+    "activities" === this.widgetType &&
+      widget.setAttribute("data-gyg-number-of-items", this.getAttribute("size") || "");
+
+    locationId && widget.setAttribute("data-gyg-location-id", locationId);
+
+    exclude &&
+      widget.setAttribute(
+        "data-gyg-excluded-tour-ids",
+        Array.of(exclude)
+          .map((it) => parseUrl(it as string)?.tourId)
+          .filter((it) => it)
+          .toString()
+      );
+
+    return widget;
+  }
+}
+
+export class GetYourGuideSearch extends GetYourGuide {
+  protected get widgetType() {
+    return "activities";
+  }
+
+  protected provideWidgetElement() {
+    const widget = super.provideWidgetElement();
+
+    const query = this.getAttribute("query");
+    const size = this.getAttribute("size");
+    const exclude = this.parseAttribute<string | string[]>("exclude");
+
+    query && widget.setAttribute("data-gyg-q", query);
+    size && widget.setAttribute("data-gyg-number-of-items", size);
+
+    exclude &&
+      widget.setAttribute(
+        "data-gyg-excluded-tour-ids",
+        Array.of(exclude)
+          .map((it) => parseUrl(it as string)?.tourId)
+          .filter((it) => it)
+          .toString()
+      );
+
+    return widget;
+  }
+}
+
+export class GetYourGuideTours extends GetYourGuide {
+  protected get widgetType() {
+    return "activities";
+  }
+
+  protected provideWidgetElement() {
+    const urls = ((val) => (Array.isArray(val) ? val : val.split(",")))(
+      this.parseAttribute<string | string[]>("urls") || ""
+    ) satisfies string[];
+
+    const tourIds = urls.map((it) => parseUrl(it)?.tourId).filter((it) => it);
+
+    const widget = super.provideWidgetElement();
+    widget.setAttribute("data-gyg-tour-ids", tourIds.toString());
+    widget.setAttribute("data-gyg-number-of-items", tourIds.length.toString());
 
     return widget;
   }
