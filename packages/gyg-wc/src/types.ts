d49d4e3b7d28307f358e5ff5bdfe80b3b87a1312
Tom Theisel <tom.theisel@gmail.com>
2023-06-17 05:05:17 +1000

    refactor(gyg-wc)!: types

--- packages/gyg-wc/src/types.ts
+++ packages/gyg-wc/src/types.ts
@@ -1,23 +1,12 @@
 export interface GetYourGuideAttributes {
   "partner-id": string;
-  widget: "activities" | "availability" | "city";
-  query: string;
   lang?: string;
 }
 
 export type GetYourGuideProps = CamelCaseKeys<GetYourGuideAttributes>;
 
-export interface GetYourGuideActivitiesAttributes extends GetYourGuideAttributes {
-  widget: "activities";
-  type: "location" | "search" | "tours";
-  exclude?: string;
-  size?: number | string;
-}
-
-export type GetYourGuideActivitiesProps = CamelCaseKeys<GetYourGuideActivitiesAttributes>;
-
 export interface GetYourGuideAvailabilityAttributes extends GetYourGuideAttributes {
-  widget: "availability";
+  url: TourUrl | string;
   theme?: "dark" | "light" | "system";
   layout?: "horizontal" | "vertical";
   currency?: string;
@@ -26,12 +15,30 @@ export interface GetYourGuideAvailabilityAttributes extends GetYourGuideAttribut
 
 export type GetYourGuideAvailabilityProps = CamelCaseKeys<GetYourGuideAvailabilityAttributes>;
 
-export interface GetYourGuideCityAttributes extends GetYourGuideAttributes {
-  widget: "city";
-  query: string;
+export interface GetYourGuideLocationAttributes extends GetYourGuideAttributes {
+  url: LocationUrl | string;
+  size?: string;
+  exclude?: string | string[];
 }
 
-export type GetYourGuideCityProps = CamelCaseKeys<GetYourGuideCityAttributes>;
+export type GetYourGuideLocationProps = CamelCaseKeys<GetYourGuideLocationAttributes>;
+
+export interface GetYourGuideSearchAttributes extends GetYourGuideAttributes {
+  query: string;
+  size?: number | string;
+  exclude?: GetYourGuideLocationAttributes["exclude"];
+}
+
+export type GetYourGuideSearchProps = CamelCaseKeys<GetYourGuideSearchAttributes>;
+
+export interface GetYourGuideToursAttributes extends GetYourGuideAttributes {
+  urls: TourUrl | TourUrl[] | string;
+}
+
+export type GetYourGuideToursProps = CamelCaseKeys<GetYourGuideToursAttributes>;
+
+export type LocationUrl = `https://www.getyourguide.com/${string}-l${string}`;
+export type TourUrl = `${LocationUrl}/${string}-t${string}`;
 
 type CamelCaseKeys<T> = {
   [Key in keyof T as CamelCase<Key>]: T[Key];
